52c0bc1b3b0b660ba4b440b28c1e77a05ddf5d73
KIM <hako5460@naver.com>
2023-12-15 15:35:09 +0900

    차원 수정

--- Model.py
+++ Model.py
@@ -15,7 +15,7 @@ mfcc_files.sort()
 
 # Module - 지역적 특성을 포착하는데 중점을 두는, 컨포터 아키텍처의 개별 구성 요소
 conv_module = ConformerConvModule(
-    dim=512,
+    dim=20,
     causal=False,  # auto-regressive 여부
     expansion_factor=2,  # depthwise convolution을 위한 차원 확장 배수
     kernel_size=31,  # 커널 크기
@@ -24,7 +24,7 @@ conv_module = ConformerConvModule(
 
 # Block - 컨포머 모듈을 포함하는더 큰 단위, 복합적인 구조
 conformer_block = ConformerBlock(
-    dim=512,  # 차원 크기
+    dim=20,  # 차원 크기
     dim_head=64,  # 어텐션 헤드의 차원
     heads=8,  # 어텐션 헤드 수
     ff_mult=4,  # 피드 포워드 멀티플라이어
@@ -43,17 +43,14 @@ for file_name in mfcc_files:
     # 데이터 형태 출력
     print("MFCC data shape:", mfcc_data.shape)
 
-    # 차원 변환 및 배치 차원 추가
-    # 여기서 데이터의 마지막 차원을 512로 맞춰야 합니다.
-    mfcc_data = np.expand_dims(mfcc_data, axis=0)  # (1, 20, 26)
-    mfcc_data = np.expand_dims(mfcc_data, axis=-1)  # (1, 20, 26, 1)
+    # MFCC 데이터 차원 변환 (20, 26) -> (26, 20)
+    mfcc_data = np.transpose(mfcc_data, (1, 0))
+
+    # MFCC 데이터에 batch_size 차원 추가 (1, 26, 20)
+    mfcc_data = np.expand_dims(mfcc_data, axis=0)
 
     # Keras 모델을 사용하기 전에 텐서로 변환
     mfcc_data = tf.convert_to_tensor(mfcc_data, dtype=tf.float32)
-
-    # Dense 레이어를 사용하여 차원 확장
-    # (batch_size, sequence_length, feature_dim) 형태로 맞추기 위해 feature_dim을 512로 확장합니다.
-    mfcc_data = Dense(512)(mfcc_data)
 
     # Convolutional Module을 통과시킨 후 결과
     conv_output = conv_module(mfcc_data) + mfcc_data
