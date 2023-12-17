cabb00d4bd7efdde2c4f61970f11712ff71639d5
KIM <hako5460@naver.com>
2023-12-17 09:24:42 +0900

    반영

--- preprocess.py
+++ preprocess.py
@@ -1,66 +1,138 @@
-import json
-import pandas as pd
 import os
 import re
+import json
+import pandas as pd
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Dense, Embedding
+from tensorflow.keras.optimizers import Adam
+import pickle
 
-def clean_and_split_sentence(sentence):
-    """
-    문장의 앞뒤 공백을 제거하고, 연속된 공백을 하나로 처리하여 어절로 분리하는 함수.
-    """
-    sentence = sentence.strip() # 앞뒤 공백 제거
-    sentence = re.sub(r'\s+', ' ', sentence) # 연속된 공백을 하나로 처리
-    return sentence.split()
+# 폴더 내 JSON 파일 목록을 가져오는 함수
+def list_json_files(folder_path):
+    return [file for file in os.listdir(folder_path) if file.endswith('.json')]
 
-def extract_dialect_standard(json_file_path):
-    with open(json_file_path, 'r', encoding='utf-8') as file:
+# 문장에서 특수문자를 제거하는 함수
+def clean_text(text):
+    return re.sub(r'[^\w\s]', '', text)
+
+# JSON 파일에서 dialect와 standard를 추출하는 함수
+def extract_sentences(json_path):
+    with open(json_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
-    dialect_standard_pairs = []
-    for segment in data['transcription']['segments']:
-        if 'dialect' in segment and 'standard' in segment:
-            dialect_standard_pairs.append((segment['dialect'], segment['standard']))
-    return dialect_standard_pairs
+    
+    sentences = []
+    for sentence in data['transcription']['sentences']:
+        # 특수문자와 구두점을 제거
+        dialect = clean_text(sentence['dialect'])
+        standard = clean_text(sentence['standard'])
+        sentences.append((dialect, standard))
+    
+    return sentences
 
-def split_sentence_to_words(dialect_sentence, standard_sentence):
-    if standard_sentence is None:
-        standard_sentence = dialect_sentence
-        #print(f"None 그대로 넣음: {dialect_sentence} -> {standard_sentence}")
-    dialect_words = clean_and_split_sentence(dialect_sentence)
-    standard_words = clean_and_split_sentence(standard_sentence)
-    if len(dialect_words) != len(standard_words):
-        print(f"문장형 그대로 넣음: {dialect_sentence}, {standard_sentence}")
-        return [(dialect_sentence, standard_sentence)]
-    return list(zip(dialect_words, standard_words))
+# 모든 JSON 파일을 처리하고 결과를 CSV 파일로 저장하는 함수
+def process_json_files_to_csv(folder_path, output_csv_path):
+    json_files = list_json_files(folder_path)
+    all_sentences = []
 
-def preprocess_and_split(df):
-    all_word_pairs = []
-    for _, row in df.iterrows():
-        word_pairs = split_sentence_to_words(row['dialect'], row['standard'])
-        for dialect, standard in word_pairs:
-            if len(dialect.split()) == 1:  # 'dialect'가 단일 단어인 경우만 포함
-                standard = ''.join(standard.split())  # 'standard'의 띄어쓰기 제거
-                all_word_pairs.append((dialect, standard))
-            else:
-                print(f"다중 단어 조합 삭제됨: {dialect} -> {standard}")
-    word_df = pd.DataFrame(all_word_pairs, columns=['dialect', 'standard'])
-    duplicated_pairs = word_df[word_df.duplicated(subset=['dialect'], keep=False)]
-    word_df.drop_duplicates(subset=['dialect'], keep='first', inplace=True)
-    return word_df
+    for json_file in json_files:
+        json_path = os.path.join(folder_path, json_file)
+        sentences = extract_sentences(json_path)
+        all_sentences.extend(sentences)
+    
+    # DataFrame 생성
+    df = pd.DataFrame(all_sentences, columns=['dialect', 'standard'])
+    # CSV 파일로 저장
+    df.to_csv(output_csv_path, index=False, encoding='utf-8-sig')
+
+# 경로 설정
+folder_path = 'dataset/Sample/label'  # JSON 파일들이 있는 폴더 경로
+output_csv_path = 'dataset/Sample/label/dialect_standard_sentences.csv'  # 저장할 CSV 파일 경로
+
+# 처리 실행
+process_json_files_to_csv(folder_path, output_csv_path)
+
+# CSV 파일 로드
+df = pd.read_csv(output_csv_path)
+
+# 데이터 검사 및 텍스트 정제 작업 수행
+df['dialect'] = df['dialect'].str.strip().str.replace(r'[^\w\s]', '', regex=True)
+df['standard'] = df['standard'].str.strip().str.replace(r'[^\w\s]', '', regex=True)
+
+# 토큰화 및 정수 인덱싱 (방언)
+tokenizer_dialect = Tokenizer()
+tokenizer_dialect.fit_on_texts(df['dialect'])
+sequences_dialect = tokenizer_dialect.texts_to_sequences(df['dialect'])
+max_length_dialect = max(len(s) for s in sequences_dialect)
+padded_sequences_dialect = pad_sequences(sequences_dialect, maxlen=max_length_dialect, padding='post')
+
+# 표준어 데이터에 대한 전처리 시작
+# 표준어 데이터에 시작과 종료 토큰 추가
+df['standard'] = '<sos> ' + df['standard'] + ' <eos>'
+
+# 토큰화 및 정수 인덱싱 (표준어)
+tokenizer_standard = Tokenizer()
+tokenizer_standard.fit_on_texts(df['standard'])
+
+# 토큰화 및 패딩 (표준어)
+sequences_standard = tokenizer_standard.texts_to_sequences(df['standard'])
+max_length_standard = max(len(s) for s in sequences_standard)
+padded_sequences_standard = pad_sequences(sequences_standard, maxlen=max_length_standard, padding='post')
+    
+# 원-핫 인코딩 (타겟 데이터)
+vocab_size_standard = len(tokenizer_standard.word_index) + 1
+decoder_target_data = np.zeros((len(sequences_standard), max_length_standard, vocab_size_standard), dtype='float32')
+
+for i, sequence in enumerate(sequences_standard):
+    for t, word_index in enumerate(sequence):
+        if word_index > 0:  # 0은 패딩을 나타내므로 제외
+            decoder_target_data[i, t, word_index] = 1.
+
+# 토크나이저 저장
+with open('tokenizer_standard.pickle', 'wb') as handle:
+    pickle.dump(tokenizer_standard, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+with open('tokenizer_dialect.pickle', 'wb') as handle:
+    pickle.dump(tokenizer_dialect, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+with open('max_length_standard.txt', 'w') as file:
+    file.write(str(max_length_standard))
+
+with open('max_length_dialect.txt', 'w') as file:
+    file.write(str(max_length_dialect))
+    
+# 모델 파라미터 설정
+embedding_dim = 256
+lstm_units = 512
+
+# 두 어휘 사전 중 큰 크기를 선택
+vocab_size = max(len(tokenizer_dialect.word_index), len(tokenizer_standard.word_index)) + 1
+    
+# 입력 시퀀스의 정의 및 처리
+encoder_inputs = Input(shape=(None,))
+encoder_embedding = Embedding(vocab_size, embedding_dim)(encoder_inputs)
+encoder_lstm = LSTM(lstm_units, return_state=True)
+encoder_outputs, state_h, state_c = encoder_lstm(encoder_embedding)
+encoder_states = [state_h, state_c]
+
+# 디코더의 정의 및 처리
+decoder_inputs = Input(shape=(None,))
+decoder_embedding_layer = Embedding(vocab_size_standard, embedding_dim)
+decoder_embedding = decoder_embedding_layer(decoder_inputs)
+decoder_lstm = LSTM(lstm_units, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
+decoder_dense = Dense(vocab_size_standard, activation='softmax')(decoder_outputs)
 
 
-def process_folder(folder_path):
-    all_pairs = []
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith('.json'):
-            file_path = os.path.join(folder_path, file_name)
-            pairs = extract_dialect_standard(file_path)
-            all_pairs.extend(pairs)
-    df = pd.DataFrame(all_pairs, columns=['dialect', 'standard'])
-    df = preprocess_and_split(df)
-    csv_file_path = os.path.join(folder_path, 'dialect_standard_word_pairs.csv')
-    df.to_csv(csv_file_path, index=False, encoding='utf-8-sig')
-    return csv_file_path
+# 모델 재정의 및 컴파일
+model = Model([encoder_inputs, decoder_inputs], decoder_dense)
+model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
-folder_path = 'dataset/Sample/label'
-csv_file_path = process_folder(folder_path)
-print(f"CSV 파일이 저장된 경로: {csv_file_path}")
+# 모델 학습
+model.fit([padded_sequences_dialect, padded_sequences_standard], decoder_target_data, batch_size=64, epochs=1, validation_split=0.2)
+
+# 모델 저장
+model.save('dialect_to_standard_translation_model.h5')
 
--- preprocess2.py
+++ preprocess2.py
@@ -1,61 +1,69 @@
-# 필요한 라이브러리 임포트
-import pandas as pd
-import numpy as np
-from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, GRU, Dropout, Dense, BatchNormalization
-from tensorflow.keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.callbacks import EarlyStopping
-import matplotlib.pyplot as plt
-from gensim.models import Word2Vec
+import pickle
+import numpy as np 
 
-# 데이터 로드
-csv_file_path = 'dataset/Sample/label/dialect_standard_word_pairs.csv'
-df = pd.read_csv(csv_file_path)
+# 토크나이저 로드
+with open('tokenizer_dialect.pickle', 'rb') as handle:
+    tokenizer_dialect = pickle.load(handle)
 
-# 토크나이저 생성 및 학습
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(df['dialect'].tolist() + df['standard'].tolist())
+with open('tokenizer_standard.pickle', 'rb') as handle:
+    tokenizer_standard = pickle.load(handle)
 
-# 시퀀스 변환 및 패딩
-dialect_seq = tokenizer.texts_to_sequences(df['dialect'])
-standard_seq = tokenizer.texts_to_sequences(df['standard'])
-dialect_padded = pad_sequences(dialect_seq, maxlen=1, padding='pre')
-standard_padded = pad_sequences(standard_seq, maxlen=1, padding='post')
+with open('max_length_standard.txt', 'r') as file:
+    max_length_standard = int(file.read())
 
-# 모델 구성
-vocab_size = len(tokenizer.word_index) + 1
-model = Sequential()
-model.add(Embedding(input_dim=vocab_size, output_dim=100, input_length=1))
-model.add(GRU(units=128, return_sequences=True))
-model.add(Dropout(0.3))
-model.add(BatchNormalization())
-model.add(GRU(units=64))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(vocab_size, activation='softmax'))
+with open('max_length_dialect.txt', 'r') as file:
+    max_length_dialect = int(file.read())
 
-# 모델 컴파일 및 훈련
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-X_train, X_val, y_train, y_val = train_test_split(dialect_padded, to_categorical(standard_padded, num_classes=vocab_size), test_size=0.2, random_state=42)
-early_stopping = EarlyStopping(monitor='val_loss', patience=10)
-model.fit(X_train, y_train, epochs=30, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping])
+# 저장된 모델 로드
+model = load_model('dialect_to_standard_translation_model.h5')
 
-# 데이터 준비: 'standard' 단어 집합에 대해서만 모델을 학습합니다.
-standard_sentences = [row.split() for row in df['standard'].tolist()]
-# 'standard' 단어에 대한 Word2Vec 모델 학습
-standard_embedding_model = Word2Vec(standard_sentences, vector_size=100, window=5, min_count=1, workers=4)
+# 번역(예측)을 위한 함수 정의
+def translate_sentence(input_sentence):
+    # 입력 문장 전처리
+    input_sequence = tokenizer_dialect.texts_to_sequences([input_sentence])
+    input_padded = pad_sequences(input_sequence, maxlen=max_length_dialect, padding='post')
+    
+    # 인코더 문맥 벡터 생성
+    encoder_context = model.layers[2](model.layers[1](input_padded))
 
-# 테스트 문장
-dialect_text = "여기에 방언 그 아방에 모둠벌초 텍스트를 입력하세요."
-words = dialect_text.split()
+    # 디코더 초기 입력(시작 토큰)
+    target_seq = np.zeros((1, 1))
+    sos_token_index = tokenizer_standard.word_index.get('<sos>', None)
+    if sos_token_index is None:
+        raise ValueError("Start-of-sentence token '<sos>' not found in tokenizer word index.")
+    target_seq[0, 0] = sos_token_index
 
-# 각 'dialect' 단어에 대해 가장 유사한 'standard' 단어 찾기
-for word in words:
-    # 'dialect' 단어가 'standard' 단어 임베딩 모델에 있는지 확인
-    if word in standard_embedding_model.wv.key_to_index:
-        similar_word = standard_embedding_model.wv.most_similar(word, topn=1)[0][0]
-        print(f"Most similar standard word for '{word}': {similar_word}")
-    else:
-        print(f"'{word}' not found in 'standard' Word2Vec model.")
+    # 번역 결과 저장
+    translated_sentence = ''
+    max_length_standard = max_length_standard
+
+    while True:
+        # 디코더에 현재 시퀀스 통과
+        output_tokens, h, c = model.layers[4](model.layers[3](target_seq), initial_state=encoder_context)
+
+        # 가장 확률 높은 단어 선택
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_word = tokenizer_standard.index_word[sampled_token_index]
+
+        # 종료 토큰 확인
+        if sampled_word == '<eos>' or len(translated_sentence) > max_length_standard:
+            break
+
+        # 결과에 단어 추가
+        translated_sentence += ' ' + sampled_word
+
+        # 타겟 시퀀스 업데이트
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = sampled_token_index
+
+        # 상태 업데이트
+        encoder_context = [h, c]
+
+    return translated_sentence.strip()
+
+# 사용 예시
+input_sentence = "여기에 제주도 방언 문장 입력"
+translated_sentence = translate_sentence(input_sentence)
+print("번역된 문장:", translated_sentence)
